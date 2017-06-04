20344581bc46301508dd69ffd28f5e1787052ac5
Maxim Zaks <maxim.zaks@gmail.com>
2017-06-04 19:49:07 +0200

    Initial Commit

---
+++ Entitas-Swift/Entitas-Swift.h
@@ -0,0 +1,19 @@
+//
+//  Entitas-Swift.h
+//  Entitas-Swift
+//
+//  Created by Maxim Zaks on 04.06.17.
+//  Copyright © 2017 Maxim Zaks. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Entitas-Swift.
+FOUNDATION_EXPORT double Entitas_SwiftVersionNumber;
+
+//! Project version string for Entitas-Swift.
+FOUNDATION_EXPORT const unsigned char Entitas_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Entitas_Swift/PublicHeader.h>
+
+
